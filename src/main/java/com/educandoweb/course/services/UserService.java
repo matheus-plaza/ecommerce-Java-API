721e02c99580ac6e7d3bf57a6b23929c6b371747
Matheus Plaza <plazachave.mp7@gmail.com>
2025-04-28 14:50:14 -0300

    User update

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -31,4 +31,18 @@ public class UserService {
 		repository.deleteById(id);
 	}
 
+	public User update(Long id, User obj) {
+		User entity = repository.getReferenceById(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+	
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setphone(obj.getphone());
+		entity.setEmail(obj.getEmail());
+	}
+	
+	
+	
 }
